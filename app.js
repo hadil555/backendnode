d2f5dfabf1f476b1b3456e635931b958eb62c112
hadil555 <farahhadil33@gmail.com>
2024-12-19 10:39:39 +0100

    Commit new version

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const scategorieRouter =require("./routes/scategorie.route")
 const dotenv=require("dotenv")
+const cors=require("cors")
 const app=express();
 const mongoose=require("mongoose")
 const categorie = require("./models/categorie")
@@ -8,11 +9,9 @@ const categorieRouter = require("./routes/categorie.route")
 const articleRouter =require("./routes/article.route")
 dotenv.config()
 app.use(express.json())
-app.use("/api/categorie", categorieRouter)
-app.use('/api/scategorie', scategorieRouter);
-app.use('/api/article', articleRouter);
+app.use(cors())
 
-app.get("/acceuil",(req,res)=>{
+app.get("/",(req,res)=>{
 
 res.send("page acceuil")
 })
@@ -21,8 +20,11 @@ mongoose.connect(process.env.DATABASECLOUD)
 .then(() => {console.log("DataBase Successfully Connected");})
 .catch(err => { console.log("Unable to connect to database", err);
 process.exit(); });
-
+app.use("/api/categorie", categorieRouter)
+app.use('/api/scategorie', scategorieRouter);
+app.use('/api/article', articleRouter);
 
 
 app.listen(process.env.PORT,()=>
 console.log(`app executer sur le port ${process.env.PORT}`))
+module.exports=app;
